f827bc307fef90653fddad021c9cbbb47101b2a3
bstaszalek <bartosz.staszalek@gmail.com>
2015-12-31 01:34:42 +0100

    Zadanie 2

---
+++ zadanie2/src/zadanie2/EnumeratorOrder.java
@@ -0,0 +1,10 @@
+package zadanie2;
+
+public class EnumeratorOrder {
+    
+    /// http://en.wikipedia.org/wiki/Breadth-first_search
+    public static final Integer BREADTH_FIRST_SEARCH = 1;
+    
+    /// http://en.wikipedia.org/wiki/Depth-first_search
+    public static final Integer DEPTH_FIRST_SEARCH = 2;
+}
---
+++ zadanie2/src/zadanie2/Zadanie2.java
@@ -0,0 +1,9 @@
+package zadanie2;
+
+public class Zadanie2 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
